bdda555838b36df6029e4985d4755e7b26d71ece
kaczormp4 <kaczkaoryginal@gmail.com>
2020-06-15 17:52:48 +0200

    gotowe

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//responsywnosc
 var bars = document.getElementById("bars");
 bars.addEventListener("click",function(){
     var header = document.getElementById("header");
@@ -14,3 +15,62 @@ bars.addEventListener("click",function(){
   //  console.log(screen.width)
     
 });
+//to do list;
+
+var butacc = document.getElementById("butacc");
+
+butacc.addEventListener("click",function(){
+    var poleacc = document.getElementById("poleacc"); 
+    var beforetext = document.getElementById("beforetext");   
+    var inputValue = poleacc.value;
+
+    var h = document.createElement("DIV");
+    var delButton = document.createElement("BUTTON");
+    var editButton = document.createElement("BUTTON");
+    
+
+    delButton.innerHTML = "✖";
+    editButton.innerHTML = "EDIT";
+    h.className = 'beforetext';
+    var t = document.createTextNode("  " + inputValue);
+
+    
+    h.appendChild(editButton);
+    h.appendChild(t);
+    
+    
+    beforetext.appendChild(h);
+
+
+    editButton.onclick = function(){
+        var edittext = document.createElement("INPUT");
+        var confirmButton = document.createElement("BUTTON");
+        h.innerHTML="";
+        h.appendChild(confirmButton);
+        h.appendChild(edittext);
+        h.appendChild(delButton);
+        confirmButton.innerHTML = "✔";
+        edittext.value = inputValue;
+
+        confirmButton.onclick = function(){
+            var inputValueChanged = edittext.value;
+            var t2 = document.createTextNode("  " + inputValueChanged);
+            editButton.innerHTML = "EDIT";
+            h.innerHTML="";
+  
+            h.appendChild(editButton);
+            h.appendChild(t2);
+            //alert('changes actepted');
+
+        }
+    }
+
+    
+    delButton.onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    
+
+    
+})
